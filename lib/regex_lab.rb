b7955fdf5ce95ef07d6e703a520c4b8bdf893492
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-27 03:42:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\d.+\b/{10})
+  if phone.match(/\d.+\b/|10|)
     true
   elsif phone.match(/\D\W/)
     false
